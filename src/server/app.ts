3db836fecc5fcc31011495b559b746a1b243b968
Igor Ilic <igor.ilic@evia.de>
2018-10-12 06:15:07 -0700

    Added print route

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { App } from 'electron';
+import { App, IpcRenderer } from 'electron';
+import { printOut } from '../lib/print';
+
 
 const PORT: number = parseInt(process.env.PORT) || 3000;
 
 export function createServer(app: App) {
+  const docPath: string = app.getPath('documents');
   const server = express();
 
   server.use(bodyParser.json());
@@ -14,10 +17,13 @@ export function createServer(app: App) {
     res.send('Hello, Evia!');
   });
 
+  server.get('/print', (req: express.Request, res: express.Response) => {
+    const text = req.query.text;
+    console.log('sebt to print ' + text);
+    printOut(docPath, text);
+  });
+
   server.listen(PORT, () => {
     console.log(`Listening on port http://localhost:${PORT}`);
   });
 }
-
-
-
